0173a11ce3cdb332783433d349aefddde0f2a210
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-04-19 09:19:37 +0530

    Update validation.js

--- formAssignment/validation.js
+++ formAssignment/validation.js
@@ -120,23 +120,6 @@ function isPassword(password) {
 	return passwordPattern.test(password);
 }  
 
-
-/*next1.onclick = () => {
-	var inputFields = document.getElementsByClassName('className');
-	var flag = true;
-	for(index=0;index<inputFields.length;index++)
-	{
-		if(inputFields[index].value=='')
-		{
-			flag=false;
-			//alert('Please fill this field');
-			inputFields[index].nextElementSibling.textContent = 'Please fill this field';
-		}
-	}
-	if(flag)
-	next1();
-}*/
-
 var form1 = document.getElementById('form-1');
 var form2 = document.getElementById('form-2');
 var form3 = document.getElementById('form-3');
@@ -152,7 +135,6 @@ var prev1 = document.getElementById('prev1');
 var prev2 = document.getElementById('prev2');
 var prev3 = document.getElementById('prev3');
 var prev4 = document.getElementById('prev4');
-
 
 function page1() {
 	if(check1 == 1 && check2 == 1 && check4 == 1 && check5 == 1) {
@@ -207,6 +189,7 @@ function page4() {
 		form5.style.left = "400px";
 	}
 }
+
 /*function page5() {
 	if(checkbox.checked == true) {
 	next4.onclick = function(){
@@ -220,42 +203,9 @@ function page4() {
   } else {
 	   alert("Please accept the terms and condition");
   }
-}
-
-/*next1.onclick = function(){
-    form1.style.left = "-400px";
-    form2.style.left = "20px";
-}
-prev1.onclick = function(){
-    form1.style.left = "20px";
-    form2.style.left = "400px";
-}
-next2.onclick = function(){
-    form2.style.left = "-400px";
-    form3.style.left = "20px";
-}
-prev2.onclick = function(){
-    form2.style.left = "20px";
-    form3.style.left = "400px";
-}
-next3.onclick = function(){
-    form3.style.left = "-400px";
-    form4.style.left = "20px";
-}
-prev3.onclick = function(){
-    form3.style.left = "20px";
-    form4.style.left = "400px";
-}
-next4.onclick = function(){
-    form4.style.left = "-400px";
-    form5.style.left = "20px";
-}
-prev4.onclick = function(){
-    form4.style.left = "20px";
-    form5.style.left = "400px";
 }*/
 
-//dob validation
+//DOB validation
 date.onchange = (event) => {
 	var date = event.target.value;//date in String
 	var dateFormat = new Date(date);//date in date
